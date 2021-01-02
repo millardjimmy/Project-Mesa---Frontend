f15165091523713215ed4570535e7ee63fdf87a4
millardjimmy <millardjimmy@gmail.com>
2021-01-02 15:22:52 -0700

    connect redux store to MoveContainer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,1 +1,10 @@
-// Must create initial state to have as a default value because there is no state in the store the first time the app runs:
+const initialState = {
+    moves: []
+}
+
+const rootReducer = (state = initialState, action => {
+    return state;
+})
+
+
+export default rootReducer;
