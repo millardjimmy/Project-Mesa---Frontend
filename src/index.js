fc331e16c332f1019306478c93e3114660fb06ea
millardjimmy <millardjimmy@gmail.com>
2021-01-02 14:48:16 -0700

    import google materialize CSS

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+// Create the Redux store:
+// const store = createStore(rootReducer)
+// Wrap App with Provider
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />
+  ,
+  document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,1 @@
+// Must create initial state to have as a default value because there is no state in the store the first time the app runs:
