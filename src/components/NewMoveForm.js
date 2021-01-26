53ef73f27b6eeb5c48ddf836e074ff07c6faa314
millardjimmy <millardjimmy@gmail.com>
2021-01-26 12:54:18 -0700

    rendered NewMoveFrom inside MoveContainer

--- src/components/NewMoveForm.js
+++ src/components/NewMoveForm.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 class NewMoveForm extends React.Component {
 
+  // Local react state for form since we are not passing any data down from this
+  state = {
+    moveName: '',
+    moveDate: null
+  }
 
   render() {
     return (
-      <div>New Move Form</div>
+      <Fragment>
+        <div className="row">
+          <div className="input-field col s6">
+            <input placeholder="Move Name" id="move_name" type="text" autocomplete="off"/>
+          <label for="move_name">Name Your Move</label>
+        </div>
+          <div className="input-field col s6">
+            <input id="move_date" type="date" autocomplete="off"/>
+            <label for="move_date">Moving On...</label>
+          </div>
+        </div>
+      </Fragment>
     )
   }
 }
--- src/containers/MoveContainer.js
+++ src/containers/MoveContainer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import MoveList from '../components/MoveList'
-
+import NewMoveForm from '../components/NewMoveForm'
 // purely presentational component
 const MoveContainer = props => {
 
     return (
       <div className="container">
-        <h2 className="card-panel cyan darken-3-text white-text" style={{fontFamily: 'Oswald'}}>My Moves</h2>
+        <h2 className="card-panel blue-grey darken-1 darken-3-text white-text" style={{fontFamily: 'Oswald'}}>My Moves</h2>
+          <NewMoveForm />
           <MoveList />
       </div>
     )
