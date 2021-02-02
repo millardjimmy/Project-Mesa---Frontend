0ff4f7463199b509cab36f091d7f167320429e6b
millardjimmy <millardjimmy@gmail.com>
2021-02-01 19:23:00 -0700

    new item is also rendering on items side bar

--- src/components/Item.js
+++ src/components/Item.js
@@ -10,7 +10,7 @@ class Item extends React.Component {
     return (
       <div className="col s12 l3">
         <div className="card small item-card z-depth-5" style={{padding: '5px'}}>
-          <span className="card-title">
+          <span className="card-title" style={{marginTop: '2px'}}>
             {name}
           </span>
           <div className="card-image" style={{width: 'auto', textAlign: 'center', marginRight: '1%', marginTop: '10%'}}>
@@ -24,22 +24,3 @@ class Item extends React.Component {
 
 
 export default Item
-
-
-
-
-
-
-// <div className="col s12 m3">
-// <div className="card">
-// <div className="card-image">
-// <img className="item-img" src={image} alt="item" />
-// </div>
-// <div className="card-content">
-// <p>{name}</p>
-// </div>
-// <div className="card-action">
-// Delete
-// </div>
-// </div>
-// </div>
