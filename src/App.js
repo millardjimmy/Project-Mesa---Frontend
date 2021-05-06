fa37afc452649906caeeeb3e07931fbdd52a8312
millardjimmy <millardjimmy@gmail.com>
2021-05-06 15:43:07 -0600

    successful fetch with accounts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
 
 class App extends React.Component {
 
   componentDidMount() {
-    fetch('http://localhost:3000/api/v1/accounts')
+    fetch('http://localhost:3000/api/v1/accounts', {
+      method: 'GET'
+  })
+    .then(reponse => reponse.json())
+    
+    .then(data => console.log(data))
   }
 
   render(){
