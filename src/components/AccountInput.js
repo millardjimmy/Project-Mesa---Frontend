a27250891fd0fbb8b9d87142c49317778b234066
millardjimmy <millardjimmy@gmail.com>
2021-05-10 13:08:59 -0600

    created two account components

---
+++ src/components/AccountInput.js
@@ -0,0 +1,17 @@
+import React from 'react' 
+
+
+class AccountInput extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Account Input
+            </div>
+        )
+
+    }
+
+}
+
+export default AccountInput
---
+++ src/components/Accounts.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+
+
+const Accounts = (props) =>  {
+
+    
+
+}
+
+
+
+// functional component
