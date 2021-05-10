6341a36850478c938830cdf75c087a7479be1fde
millardjimmy <millardjimmy@gmail.com>
2021-05-10 13:09:31 -0600

    created account container

---
+++ src/containers/AccountsContainer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+class AccountsContainer extends React.Component {
+
+    render () {
+
+        return (
+
+            <div>
+                Accounts Container
+            </div>
+        )
+    }
+
+
+
+}
+
+export default AccountsContainer
