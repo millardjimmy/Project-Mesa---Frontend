d1a4d433f7ccbbc0a73747f4b34012397bf26346
millardjimmy <millardjimmy@gmail.com>
2021-05-17 16:51:13 -0600

    altered path to find accounds, now based ID

--- src/components/Account.js
+++ src/components/Account.js
@@ -6,7 +6,9 @@ import TransactionContainer from '../containers/TransactionsContainer'
 
 const Account = (props) => {
 
-    let account = props.accounts[props.match.params.id - 1 ]
+    // let account = props.accounts[props.match.params.id - 1 ]
+    
+    let account = props.accounts.filter(account => account.id == props.match.params.id)[0]
     
     console.log(account)
 
