1db44e9e6ab48ccaede51c198e184b29e0c7e0e4
millardjimmy <millardjimmy@gmail.com>
2021-07-26 09:55:30 -0600

    Restructure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import AccountsContainer from './containers/AccountsContainer'
 
-class App extends React.Component {
-
-  render(){
-    return (
-      <div className="App">
-        <AccountsContainer/>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-
-
-
-
-export default (App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-
-import reportWebVitals from './reportWebVitals';
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux';
-import {BrowserRouter as Router} from 'react-router-dom';
-import accountReducer from './reducers/accountReducer'
-
-
 import App from './App';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-
-let store = createStore(accountReducer, composeEnhancers(applyMiddleware(thunk)))
-
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
